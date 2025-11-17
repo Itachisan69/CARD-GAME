8a50a76433b346128f8ef929d2d92753c1925255
MohamedSliti <slitimohamed419@gmail.com>
2025-11-17 09:24:23 +0100

    Created Card Prefab

---
+++ CARD GAME/Assets/_Scripts/Views/CardView.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CardView : MonoBehaviour
+{
+    [SerializeField] TMP_Text title;
+    [SerializeField] TMP_Text description;
+    [SerializeField] TMP_Text mana;
+    [SerializeField] SpriteRenderer imageSR;
+    [SerializeField] GameObject wrapper;
+}
